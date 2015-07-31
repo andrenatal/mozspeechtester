acc6d4ee5c5e6d0a29b5715d2f0c4d0e5cf6da6b
Andre Natal <anatal@gmail.com>
2015-07-30 17:00:44 -0700

    Offline tts and digits

--- vdialer.js
+++ vdialer.js
@@ -3,31 +3,32 @@
 var speakbtn = document.querySelector("#speak");
 var sayaudio = document.querySelector("#say");
 var recognizing = false;
+var final_transcript = "";
 
 document.querySelector("#listening").style.display = 'none';
 //document.querySelector("#lblstatus").style.display = 'none';
 
 
 var speechrecognitionlist = new SpeechGrammarList();
-speechrecognitionlist.addFromString  ( "#JSGF V1.0; grammar test; public <simple> =  please call my wife  | firefox what is the weather today|  how many messages i have | please text josh and say i am late  | firefox check my battery ; ", 1 );     
-var recognition = new SpeechRecognition();     
+speechrecognitionlist.addFromString  ( " #JSGF V1.0; grammar test; <numbers> = zero | one | two | three | four | five | six | seven | eight | nine; public <final_digits> = <numbers>+;", 1 );
+var recognition = new SpeechRecognition();
 
 console.log("speakbtn");
 
-speakbtn.onclick = function () 
-{ 
-  recognizing = true;
-  say("How can I help you?"); 
-}  
-
-
-function onendspeak() 
+speakbtn.onclick = function ()
 {
-    if (!recognizing) 
+  recognizing = true;
+  say("How can I help you?");
+}
+
+
+function onendspeak()
+{
+    if (!recognizing)
     {
         return;
     }
-    changelabel("Listening...");              
+    changelabel("Listening...");
     document.querySelector("#fox").style.display = 'none';
     document.querySelector("#listening").style.display = 'block';
     document.querySelector("#lblstatus").style.display = 'block';
@@ -36,92 +37,92 @@ function onendspeak()
     document.querySelector("#messages").style.display = 'none';
     document.querySelector("#tel").style.display = 'none';
     document.querySelector("#fox").style.display = 'none';
-    document.querySelector("#text").style.display = 'none';            
-    document.querySelector("#battery").style.display = 'none'; 
+    document.querySelector("#text").style.display = 'none';
+    document.querySelector("#battery").style.display = 'none';
 
     console.log('starting')
     recognition.start();
-    recognition.onresult = function(event) 
+    recognition.onresult = function(event)
     {
           recognizing = false;
           document.querySelector("#listening").style.display = 'none';
-          
-          var interim_transcript = '';
+
+          var final_transcript = '';
           var score = '';
 
           // Assemble the transcript from the array of results
           for (var i = event.resultIndex; i < event.results.length; ++i) {
               if (event.results[i].isFinal) {
-                  console.log("recognition.onresult : isFinal");                                
+                  console.log("recognition.onresult : isFinal");
                   final_transcript += event.results[i][0].transcript;
               } else {
-                  console.log("recognition.onresult : not isFinal");                                                                
+                  console.log("recognition.onresult : not isFinal");
                   interim_transcript += event.results[i][0].transcript;
                   score = event.results[i][0].confidence;
               }
           }
 
-          changelabel(interim_transcript);
-          console.log("interim_transcript");
+          changelabel(final_transcript);
+          console.log("interim_transcript: " + final_transcript);
 
-
-          if (interim_transcript.indexOf('weather') > -1)
+/*
+          if (final_transcript.indexOf('weather') > -1)
           {
             say("Today it is 75 degrees and sunny in Mountain View");
             document.querySelector("#weather").style.display = 'none';
             document.querySelector("#messages").style.display = 'none';
             document.querySelector("#tel").style.display = 'none';
             document.querySelector("#fox").style.display = 'none';
-            document.querySelector("#text").style.display = 'none';            
-            document.querySelector("#battery").style.display = 'none';                        
+            document.querySelector("#text").style.display = 'none';
+            document.querySelector("#battery").style.display = 'none';
 
-            document.querySelector("#weather").style.display = 'block'; 
-
-          }
-
-         if (interim_transcript.indexOf('messages') > -1)
-          {
-            say("You have 3 texts and 20 unread emails."); 
-            document.querySelector("#weather").style.display = 'none';
-            document.querySelector("#messages").style.display = 'none';
-            document.querySelector("#tel").style.display = 'none';
-            document.querySelector("#fox").style.display = 'none';
-            document.querySelector("#text").style.display = 'none';            
-            document.querySelector("#battery").style.display = 'none';                        
-
-            document.querySelector("#messages").style.display = 'block'; 
+            document.querySelector("#weather").style.display = 'block';
 
           }
 
-         if (interim_transcript.indexOf('text') > -1)
+         if (final_transcript.indexOf('messages') > -1)
           {
-            say("Ok. I am texting Josh saying you are late"); 
+            say("You have 3 texts and 20 unread emails.");
             document.querySelector("#weather").style.display = 'none';
             document.querySelector("#messages").style.display = 'none';
             document.querySelector("#tel").style.display = 'none';
             document.querySelector("#fox").style.display = 'none';
-            document.querySelector("#text").style.display = 'none';            
-            document.querySelector("#battery").style.display = 'none';                        
+            document.querySelector("#text").style.display = 'none';
+            document.querySelector("#battery").style.display = 'none';
 
-            document.querySelector("#text").style.display = 'block';            
+            document.querySelector("#messages").style.display = 'block';
 
-          }   
+          }
 
-         if (interim_transcript.indexOf('battery') > -1)
+         if (final_transcript.indexOf('text') > -1)
           {
-            say("I still have 75 percent of battery remaining."); 
+            say("Ok. I am texting Josh saying you are late");
             document.querySelector("#weather").style.display = 'none';
             document.querySelector("#messages").style.display = 'none';
             document.querySelector("#tel").style.display = 'none';
             document.querySelector("#fox").style.display = 'none';
-            document.querySelector("#text").style.display = 'none';            
-            document.querySelector("#battery").style.display = 'none';                        
+            document.querySelector("#text").style.display = 'none';
+            document.querySelector("#battery").style.display = 'none';
 
-            document.querySelector("#battery").style.display = 'block'; 
+            document.querySelector("#text").style.display = 'block';
 
-          }          
+          }
 
-          if (interim_transcript.indexOf('call') > -1)
+         if (final_transcript.indexOf('battery') > -1)
+          {
+            say("I still have 75 percent of battery remaining.");
+            document.querySelector("#weather").style.display = 'none';
+            document.querySelector("#messages").style.display = 'none';
+            document.querySelector("#tel").style.display = 'none';
+            document.querySelector("#fox").style.display = 'none';
+            document.querySelector("#text").style.display = 'none';
+            document.querySelector("#battery").style.display = 'none';
+
+            document.querySelector("#battery").style.display = 'block';
+
+          }
+
+          if (final_transcript.indexOf('call') > -1)
           {
             say("Ok. I am calling Samanta.")
 
@@ -129,40 +130,47 @@ function onendspeak()
             document.querySelector("#messages").style.display = 'none';
             document.querySelector("#tel").style.display = 'none';
             document.querySelector("#fox").style.display = 'none';
-            document.querySelector("#text").style.display = 'none';            
-            document.querySelector("#battery").style.display = 'none';                        
+            document.querySelector("#text").style.display = 'none';
+            document.querySelector("#battery").style.display = 'none';
 
-            document.querySelector("#tel").style.display = 'block'; 
+            document.querySelector("#tel").style.display = 'block';
 
-            setTimeout(function(){      
+            setTimeout(function(){
 
                 searchcontact("Samanta");
-            
-             }, 5000);   
+
+             }, 5000);
 
 
           }
+          */
     };
 }
 
 function say(phrase)
 {
     changelabel(phrase);
-    urlaudio = "http://speechan.cloudapp.net/weblayer/synth.ashx?lng=en&msg=" + phrase;
- //   sayaudio.setAttribute("src","http://speechan.cloudapp.net/weblayer/synth.ashx?lng=en&msg=" + phrase); 
-
-    setTimeout(function(){      
-
-   var e = document.createElement("audio");
-    e.src = urlaudio;
-    e.setAttribute("autoplay", "true");
-    //e.currentTime = 0;
-    e.addEventListener("ended", onendspeak);
 
 
-     }, 500);
+       setTimeout(function() {
+      var speechSynthesisUtterance = new SpeechSynthesisUtterance(phrase);
+      // XXX: Language should be detected based on system language.
+      speechSynthesisUtterance.lang = 'en';
+      speechSynthesisUtterance.addEventListener('end', (function() {
+        // Enable the speak button.
+        //this.setSpeakButtonState(false);
+        onendspeak();
+        // If we're waiting for a command, start listening.
+        if (aIsWaitingForCommandResponse) {
+          this.listen();
+        }
+      }).bind(this));
+      speechSynthesis.speak(speechSynthesisUtterance);
+    }.bind(this),
+    100);
 
- 
+
+
 
 }
 
@@ -199,24 +207,24 @@ function call(number)
       // Telephony object
     var tel = navigator.mozTelephony;
     // Place a call
-    var telCall = tel.dial(number);  
+    var telCall = tel.dial(number);
     telCall.onactive = function(e) {
-        changelabel("");          
+        changelabel("");
         window.console.log('Connected!');
     }
     telCall.ondisconnected = function(e) {
-        changelabel("");          
+        changelabel("");
         window.console.log('Disconnected!');
         // update call history
     }
     telCall.onerror = function(e) {
-        changelabel("");          
+        changelabel("");
         window.console.error(e);
     }
 }
 
 function changelabel(str){
-  document.querySelector("#lblstatus").style.display = 'block';  
+  document.querySelector("#lblstatus").style.display = 'block';
   document.querySelector("#lblstatus").innerHTML = str;
 }
 
